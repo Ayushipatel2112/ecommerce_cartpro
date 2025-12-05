61f2fd0ea760f8533de6858a254ff087385b77fe
Ayushi <dalsaniyauma@gmail.com>
2025-12-05 14:32:42 +0530

    Add this file via upload

--- CartProWebApp/Checkout.aspx.cs
+++ CartProWebApp/Checkout.aspx.cs
@@ -1,18 +1,1 @@
-using System;
-using System.Web.UI;
 
-namespace CartProWebApp
-{
-    public partial class Checkout : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            
-        }
-
-        protected void btnPlaceOrder_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("ThankYou.aspx");
-        }
-    }
-}
